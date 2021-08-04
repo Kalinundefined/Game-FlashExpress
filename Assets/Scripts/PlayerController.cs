aec70c4ab7e50bb8211b8867c282ba07e002b829
Morehab <artinee_morehab@outlook.com>
2021-08-04 14:06:35 +0800

    update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 1.0f;
+    public Text dialogText;
+    public GameObject dialogBox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        ShowText();
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * maxSpeed * Time.deltaTime);
+    }
+
+    private void ShowText()
+    {
+        if (Input.GetKey("e"))
+        {
+            dialogBox.SetActive(true);
+            dialogText.text = "This is text.";
+        }
+    }
+}
